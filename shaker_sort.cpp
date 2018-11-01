6b19a0041bac819f746223ef7bf352135ab67005
Maria Duvanova <d_muriam@inbox.ru>
2018-11-01 19:37:03 +0700

    Update shaker_sort.cpp

--- shaker_sort.cpp
+++ shaker_sort.cpp
@@ -24,32 +24,32 @@ int main()
 	for (right=SIZE-1, left=0, border=-1; border!=0;)      // устанавливаю правую и левую границы
 	{
 		border = 0;
-		for (int i=left; i<right; i++)         // двигаюсь слева направо
+		for (int i=left; i<right; i++)               // двигаюсь слева направо
 		{
 			if (array[i] > array[i+1])           // если текущий элемент больше следующего
 			{ 
-				temp = array[i];                   // 
-				array[i] = array[i+1];			       // меняю их местами
-				array[i+1] = temp;				         //
-				border = i;                        // устанавливаю метку последней перестановки 
+				temp = array[i];             // 
+				array[i] = array[i+1];       // меняю их местами
+				array[i+1] = temp;           //
+				border = i;                  // устанавливаю метку последней перестановки 
 			}
 		}
-		right = border;                        // запоминаю правую границу 
-		for (int i=right; i>left; i--)         // двигаюсь справа налево
+		right = border;                              // запоминаю правую границу 
+		for (int i=right; i>left; i--)               // двигаюсь справа налево
 		{
 			if (array[i-1] > array[i])           // если текущий элемент меньше следующего
 			{
-				temp = array[i];                   // 
-				array[i] = array[i-1];			       // меняю их местами
-				array[i-1] = temp;				         //
-				border = i;                        // устанавливаю метку последней перестановки
+				temp = array[i];             // 
+				array[i] = array[i-1];       // меняю их местами
+				array[i-1] = temp;	     //
+				border = i;                  // устанавливаю метку последней перестановки
 			}
 		}
-		left = border;                         // запоминаю границу
+		left = border;                               // запоминаю границу
 	}
         
     cout << "шейкерная сортировка" << endl;
-    for(int i=0; i < SIZE; i++)
+    for (int i=0; i < SIZE; i++)
         cout << array[i] << " ";
     cout << "\n\n";
 
