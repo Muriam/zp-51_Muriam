3325d4a35c509892464e8fdcd7a5f9acb052dcb3
Maria Duvanova <d_muriam@inbox.ru>
2019-10-08 14:40:33 +0600

    Update lab_1_optimization.cpp

--- lab_1_optimization.cpp
+++ lab_1_optimization.cpp
@@ -13,138 +13,152 @@
 #include <cstring>
 #define SIZE 63     
      
-using namespace std;
+using std::cout;
+using std::endl;
 
-int array[SIZE];
-int array2[SIZE];
-int n;
-     
-void random_array(int (&array)[SIZE]);
-void insertion_sort(int (&array)[SIZE]);
-void Merge(int (&array)[SIZE], int first, int last);        //слияние массивов
-void merge_sort(int (&array)[SIZE], int first, int last);   //рекурсивная процедура сортировки
-     
-     
+int _array1[SIZE];
+int _array2[SIZE];
+int n = SIZE - 1;
+long int time1;
+
+void random_array(int* array);
+long int insertion_sort(int* array);
+void Merge(int* array, int first, int last);        //слияние массивов
+void merge_sort(int* array, int first, int last);   //рекурсивная процедура сортировки
+
+
 int main()
-{
-    //    long int start_time = clock(); 			// начальное время
-     
-    setlocale(LC_ALL, "rus");	
-     
-    srand(time(0));        
-     
-   
-    random_array(array);                 	       	//исходный массов
-    cout << "\n\n";	
- 
-     
-    memcpy(array2, array, sizeof(int) * SIZE);          //копия исходного массова
-    cout << "копия исходного массива" << "\n";
-    for (int i = 0; i < SIZE; i++)
-    {
-    	cout << array2[i] << "  ";
-    }	
-    cout << "\n";
-      
-  
-    insertion_sort(array);               	    	//вызов сортировки вставками
-    for (int i = 0; i < SIZE; i++)
-    {
-    	cout << array[i] << "  ";
-    }
-    cout << "\n";	
+{	
 
-       
-    merge_sort(array2, 1, n);             		//вызов сортировки слиянием
-    for (int i = 0; i < SIZE; i++)
-    {
-    	cout << array[i] << "  ";
-    }  
-     
-             
-     
-    /*
-        long int end_time = clock();                    // конечное врем¤	
-        long int time1 = end_time - start_time;         // искомое врем¤	
-        cout << "\n" << "время работы программы " << time1 << " миллисекунд";
-        float time2 = (float)time1 / 1000;
-        cout << "\n" << "То есть, это составляет " << time2 << " секунд";
-    */
-     
-    getch();
-    return 0;
+    setlocale(LC_ALL, "rus");
+
+    srand(time(0));
+
+
+
+	random_array(_array1);                 	       						//исходный массов
+//	cout << "\n\n";                      
+
+
+
+	memcpy(_array2, _array1, sizeof(int) * SIZE);          				//копия исходного массова
+/*	cout << "копия исходного массива" << "\n";
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << _array2[i] << " ";
+	}
+	cout << "\n";
+ЗАКОММЕНТИРОВАНО, ЧТОБ ДОЛГО НЕ ЖДАТЬ ВЫВОД ОГРОМНОГО МАССИВА НА ЭКРАН */
+	
+	
+	
+	insertion_sort(_array1);                							//вызов сортировки вставками	
+/*	for (int i = 0; i < SIZE; i++)
+	{
+		cout << _array1[i] << " ";
+	}
+	cout << "\n";
+ЗАКОММЕНТИРОВАНО, ЧТОБ ДОЛГО НЕ ЖДАТЬ ВЫВОД ОГРОМНОГО МАССИВА НА ЭКРАН */
+
+
+
+//	cout << "\nотсортировано слиянием" << endl;
+	merge_sort(_array2, 0, n);             								//вызов сортировки слиянием
+/*	for (int i = 0; i < SIZE; i++)
+	{
+		cout << _array2[i] << " ";
+	}
+	cout << "\n\n";
+ЗАКОММЕНТИРОВАНО, ЧТОБ ДОЛГО НЕ ЖДАТЬ ВЫВОД ОГРОМНОГО МАССИВА НА ЭКРАН */
+	
+	cout << "\n" << "при размере массива = " << SIZE << " элементов" << "\n" ;
+	cout << "время работы сортировки вставками = " << time1 << " мсек" << "\n\n" ;
+	
+
+
+
+	getch();
+	return 0;
 }
-     
-     
-void random_array(int (&array)[SIZE])
+
+
+void random_array(int* array)
 {
-    cout << "исходный массив" << endl;
-    for (int i=0; i<SIZE; i++) 
-    {
-        array[i] = rand()%100;
-        cout << array[i] << " ";
-    }
+	//cout << "исходный массив" << endl;	
+	for (int i = 0; i < SIZE; i++)
+	{
+		array[i] = rand() % 100;
+		//cout << array[i] << " ";              ЗАКОММЕНТИРОВАНО, ЧТОБ ДОЛГО НЕ ЖДАТЬ ВЫВОД ОГРОМНОГО МАССИВА НА ЭКРАН 
+	}
+	
 }
-     
-     
-void insertion_sort(int (&array)[SIZE])
+
+
+long int insertion_sort(int* array)
 {
-    cout << "\nотсортировано вставками" << endl; 
-     
-    for (int i = 1; i < SIZE; i++)
-    {
-    	int temp = array[i];
-    	int j = i - 1;
-    	while (j >= 0 && temp < array[j])
-    	{
-            array[j + 1] = array[j];
-            j--;
-    	}
-    	array[j + 1] = temp;
-    }
+//	cout << "\nотсортировано вставками" << endl;
+
+long int start_time = clock(); 						// начальное время		//*************
+
+	for (int i = 1; i < SIZE; i++)
+	{
+		const int temp = array[i];
+		int j = i - 1;
+		while (j >= 0 && temp < array[j])
+		{
+			array[j + 1] = array[j];
+			j--;
+		}
+		array[j + 1] = temp;
+	}
+	
+long int end_time = clock();                    // конечное время				//*************
+time1 = end_time - start_time;                  // искомое время в мсек  		     //*************
+//float time2 = (float)time1 / 1000;		   // это в секундах				//*************
+
+return time1;															//*************	
 }
-     
- 
-void Merge(int (&array)[SIZE], int first, int last)
-{ 
-    
-    int middle, start, finall, j;
-     
-    int *mas = new int[SIZE];
-     
-    middle = (first + last) / 2;			//вычисление среднего элемента
-    start = first; 					//начало левой части
-    finall = middle + 1; 				//начало правой части
-     
-    for (j = first; j <= last; j++) 			//выполнять от начала до конца
-    	if ((start <= middle) && ((finall > last) || (array[start] < array[finall])))
-    	{
-    	    mas[j] = array[start];
-       	    start++;
-    	}
-    	else
-    	{
-    	    mas[j] = array[finall];
-       	    finall++;
-    	}
-     
-    //возвращение результата в список
-    for (j = first; j <= last; j++) 
-        array[j] = mas[j];
-     
-    delete [] mas;	
-}
-     
-     
-void merge_sort(int (&array)[SIZE], int first, int last)
+
+
+void Merge(int* array, int first, int last)
 {
-    cout << "\nотсортировано слиянием" << endl;
-    { 
-    	if (first < last)
-    	{
-    	    merge_sort(array, first, (first+last)/2); 	//сортировка левой части
-    	    merge_sort(array, (first+last)/2+1, last); 	//сортировка правой части
-    	    Merge(array, first, last); 			//слияние двух частей
-    	}
-    }
-};  
+	int middle, start, finall, j;
+
+	int *mas = new int[SIZE];
+
+	middle = (first + last) / 2;						//вычисление среднего элемента
+	start = first; 								//начало левой части
+	finall = middle + 1; 							//начало правой части
+
+	for (j = first; j <= last; j++) 					//выполнять от начала до конца
+		if ((start <= middle) && ((finall > last) || (array[start] < array[finall])))
+		{
+			mas[j] = array[start];
+			start++;
+		}
+		else
+		{
+			mas[j] = array[finall];
+			finall++;
+		}
+
+	//возвращение результата в список
+	for (j = first; j <= last; j++)
+		array[j] = mas[j];
+
+	delete[] mas;
+}
+
+
+void merge_sort(int* array, int first, int last)
+{
+	//cout << "\nотсортировано слиянием" << endl;
+	{
+		if (first < last)
+		{
+			merge_sort(array, first, (first + last) / 2); 		//сортировка левой части
+			merge_sort(array, (first + last) / 2 + 1, last); 	     //сортировка правой части
+			Merge(array, first, last); 						//слияние двух частей
+		}
+	}
+};
